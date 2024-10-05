5bf0454304c46ab6dc23691dae6ecce95ad733d3
Pranjal Rastogi <126326239+pranjal-rastogi@users.noreply.github.com>
2024-10-05 06:34:04 +0530

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-// Note: The AggregatorV3Interface might be at a different location than what was in the video!
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
